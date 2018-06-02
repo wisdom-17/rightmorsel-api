8def67d970605c157587b70c130db3903f9203ce
Saeed <saeed17@gmail.com>
2018-06-02 12:00:23 +0100

    Correctly instantiated validator in OutletTableWriterTest.

--- tests/AppBundle/Database/OutletTableWriterTest.php
+++ tests/AppBundle/Database/OutletTableWriterTest.php
@@ -4,34 +4,40 @@ namespace Tests\AppBundle\Database;
 
 use AppBundle\Database\OutletTableWriter;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Validation;
 use Doctrine\ORM\EntityManagerInterface;
 
 class OutletTableWriterTest extends TestCase
 {
+
+    private $validator;
+
+    private $mockEm;
+
+    public function __construct()
+    {
+        $this->validator    = Validation::createValidatorBuilder()->enableAnnotationMapping()->getValidator();
+        $this->mockEm       = $this->getMockBuilder(EntityManagerInterface::class)
+            ->disableOriginalConstructor()->getMock();
+    }
+
     public function testSuccessfulInsertOutlet()
     {
-
-        $mockValidator  = $this->getMockBuilder(ValidatorInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $mockEm         = $this->getMockBuilder(EntityManagerInterface::class)
-            ->disableOriginalConstructor()->getMock();
-
-
-        $outletTableWriter  = new OutletTableWriter($mockValidator, $mockEm);
+        $outletTableWriter  = new OutletTableWriter($this->validator, $this->mockEm);
         $response           = $outletTableWriter->insertOutlet(
             'Test Outlet', null, 32, 'Dawn Crescent', 'Stratford', 'London', null, 'E15 3NT'
         );
 
         $this->assertEquals(201, $response->getStatusCode());
-
-
     }
 
     public function testUnsuccessfulInsertOutlet()
     {
+        $outletTableWriter  = new OutletTableWriter($this->validator, $this->mockEm);
+        $response           = $outletTableWriter->insertOutlet(
+            '','', '', '', '', '', '', 'EXX 1XX'
+        );
 
+        $this->assertEquals(422, $response->getStatusCode());
     }
 }
