4eb6679e4c5bdd712a9615cb2b858d1e794f9779
Saeed Bhuta <saeed.bhuta@annalect.com>
2018-09-14 15:53:54 +0100

    Comment and improve test db name.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -12,7 +12,9 @@ class AppBundle extends Bundle
 	public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        // adding a custom compiler pass to change services to public when testing. 
 
+        // See more: https://www.tomasvotruba.cz/blog/2018/05/17/how-to-test-private-services-in-symfony/#down-the-smelly-rabbit-hole
         $container->addCompilerPass(new PublicForTestsCompilerPass());
     }
 }
